cc069553bea52792d0fbdb9745ced0a9ac0532c4
Timothy Miller <timothy.miller@icloud.com>
2014-01-27 00:04:42 -0500

    Micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,5 +14,8 @@ class Micropost < ActiveRecord::Base
   
   attr_accessible :content
   
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :user_id, presence: true
+  
   default_scope order: 'microposts.created_at DESC'
 end
