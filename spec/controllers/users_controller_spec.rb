dcacbb3a5da2a1a0b1431c23018fefafb704c6d5
Timothy Miller <timothy.miller@icloud.com>
2014-02-06 17:16:59 -0500

    Pages for following/followers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -327,5 +327,39 @@ describe UsersController do
       end
     end
   end
+  
+  describe "follow pages" do
+    describe "when not signed in" do
+      it "should protect 'following'" do
+        get :following, id: 1
+        response.should redirect_to(signin_path)
+      end
+      
+      it "should protect 'followers'" do
+        get :followers, id: 1
+        response.should redirect_to(signin_path)
+      end
+    end
+    
+    describe "when signed in" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:other_user) { FactoryGirl.create(:user) }
+      
+      before do
+        test_sign_in(user)
+        user.follow!(other_user)
+      end
+      
+      it "should show user following" do
+        get :following, id: user
+        response.should have_selector('a', href: user_path(other_user), content: other_user.name)
+      end
+      
+      it "should show user followers" do
+        get :followers, id: other_user
+        response.should have_selector('a', href: user_path(user), content: user.name)
+      end
+    end
+  end
 
 end
